8a2837b096120671b95f98bf8e1e8f151bf0057f
Mauro Benck Homrich Junior <182110041@senacinformatica.br>
2022-07-12 21:15:37 -0300

    NLogin/Services/UsuarioServices: Criado cadUsuarioServicos e getByUsuarioServicos

--- src/br/com/senactech/NLogin/services/UsuarioServicos.java
+++ src/br/com/senactech/NLogin/services/UsuarioServicos.java
@@ -3,13 +3,24 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package br.com.senactech.NLogin.services;
+import br.com.senactech.NLogin.dao.DAOFactory;
+import br.com.senactech.NLogin.dao.UsuarioDAO;
+import br.com.senactech.NLogin.model.Usuario;
 import java.sql.SQLException;
-
-
 /**
  *
  * @author Mauro B H Jr
  */
 public class UsuarioServicos {
+    
+    public void cadUsuarioServicos(Usuario uVO) throws SQLException{
+        UsuarioDAO uDAO = DAOFactory.getUsuarioDAO();
+        uDAO.cadUsuarioDAO(uVO);
+    }
+    
+    public Usuario getByUsuarioServicos(String user) throws SQLException{
+        UsuarioDAO uDAO = DAOFactory.getUsuarioDAO();
+        return uDAO.getByUsarioDAO(user);
+    }
     
 }
