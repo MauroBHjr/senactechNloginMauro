d6d941ce586172a2a944d3237d321132d8ccdea8
Mauro Benck Homrich Junior <182110041@senacinformatica.br>
2022-07-12 21:10:01 -0300

    Criada Classe DAOFactory no Pacote DAO, criada classe UsuarioServicos no pacote Services

---
+++ src/br/com/senactech/NLogin/services/UsuarioServicos.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.senactech.NLogin.services;
+import java.sql.SQLException;
+
+
+/**
+ *
+ * @author Mauro B H Jr
+ */
+public class UsuarioServicos {
+    
+}
